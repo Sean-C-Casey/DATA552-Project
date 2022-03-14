fe7565bfd570a30878b6d27caf39ef0cc572b6c3
Sean Casey <sccasey04@gmail.com>
2022-03-14 13:38:36 -0700

    Write code to filter out twitter links

--- reddit.py
+++ reddit.py
@@ -1,5 +1,5 @@
-from ast import Sub
 import praw
+import re
 
 from settings import REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET
 
@@ -14,27 +14,41 @@ class RedditConnection(object):
         )
         self._subredit = self._client.subreddit("UkrainianConflict")
     
-    def get_top_n(self, n: int) -> list["praw.models.Submission"]:
+    def get_top_n(self, n: int, skip_twitter: bool = True) -> list["praw.models.Submission"]:
         submissions = []
         count = 0
-        posts = list(self._subredit.hot(limit=2*n))
+        query_limit = max(4, 3 * n)
+        posts = list(self._subredit.hot(limit=query_limit))
         index = 0
-        while count < n:
+        while count < n and index < query_limit:
             post = posts[index]
             index += 1
+
+            if skip_twitter and self.is_twitter(post):
+                continue
+
             if not post.stickied:
                 submissions.append(post)
                 count += 1
         return submissions
+    
+    @staticmethod
+    def is_twitter(submission: "praw.models.Submission") -> bool:
+        regex = "^https?:\/\/(.*\.[a-z]*)\/.*$"
+        url = submission.url
+        matches = re.match(regex, url)
+        try:
+            base_url = matches[1]
+            return "twitter" in base_url.lower()
+        except IndexError:
+            return False
 
 
 def test():
     reddit = RedditConnection()
 
-    Submission = praw.models.Submission
-    post: Submission
     for post in reddit.get_top_n(3):
-        print(post.title, "\n")
+        print(post.url, "\n")
 
 
 if __name__ == "__main__":
